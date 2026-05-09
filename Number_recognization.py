ab66c1797fd04b992627ffe1bad42da68f86397a
Sampanna-225 <sampanna339@gmail.com>
2026-05-09 19:06:45 +0545

    Adding more logic in Must read

--- Number_recognization.py
+++ Number_recognization.py
@@ -8,11 +8,11 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from NetBrain import NeuralBrain
 
-x1 = np.array([[1,0,1,1]]) #2+3 on binary
-y1 = np.array([[1,0,1]]) # five in binary
+x1 = np.array([[1,0,1,1],[0,1,0,1]]) #2+3 and 1+1 on binary
+y1 = np.array([[1,0,1],[0,1,0]]) # five in binary
 
 model = NeuralBrain(x1,y1,input_size=4 , hidden_size=10 , output_size=3,lr=0.01)
 model.train(1000000)
-final=model.forward_propagation(x1)
+final=model.forward_propagation([[1,0,1,1]])
 print(final)
 model.graphplot()
