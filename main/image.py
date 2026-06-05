638a380c3aac5986c476d8d4501f1336cea8bf94
Sampanna-225 <sampanna339@gmail.com>
2026-06-05 21:45:05 +0545

    Center of Mass theory

--- main/image.py
+++ main/image.py
@@ -27,16 +27,44 @@ def image_converter(pathname:str) -> np.ndarray:
         #   +----------------------+
         #          w (Width)
 
-    croped = res_up[y:y-h, x:x+h]
+    croped = res_up[y:y+h, x:x+w]
     
+    max_dim = max(h, w) #Selects the highest dimension of the cropped peice so that the padding fits all the data selected
+    
+    scalling = 20.0 / max_dim
 
-   
-    resize = cv2.resize(res_up, (20,20), interpolation=cv2.INTER_AREA) # 20 to compensate the padding
+    h_new , w_new = max(1,int(h* scalling)),max(1,int(w* scalling)) #int to elimiate teh decimals as pixels dimensions are in the form of +ve integers and avoid zero dimentions.
 
-    h ,w = resize.shape
-    h_pad , w_pad = 4, 4# 4 pixels of padding on all ends so the final becomes (20+8,20+8)
-    padded = cv2.copyMakeBorder(resize, h_pad, h_pad, w_pad, w_pad, cv2.BORDER_CONSTANT, value=0)#padds the images with value=0(black)
+    resize = cv2.resize(croped, (w_new,h_new), interpolation=cv2.INTER_AREA) # near 20 padding to compensate the padding
 
-    normalize = padded.astype(np.float64)/255.0
+    empty_cavas = np.zeros((28,28),dtype=np.uint8)
+    M = cv2.moments(resize)
+    if M["m00"] != 0:
+        cx , cy = M["m10"]/M["m00"] , M["m01"]/M["m00"]
+    else:
+        cx , cy = w_new / 2 , h_new / 2 #get the middle coordinates if the above condition fails.
 
-    return normalize.reshape(-1,784) # production fromat
+    #Finding the start if the top corner coodinates
+    start_x = int(14-cx)
+    start_y = int(14-cy)
+
+    #Determining the slice limit for empty canvas:
+    x1 , x2 = max(0,start_x) , min(28,start_x+w_new)
+    y1 , y2 = max(0,start_y) , min(28,start_y+h_new) 
+
+    #Determining the slice limit for the source:
+    #Handles overflows:
+    #finds the start of the source from origin. If the start of canvas is greater than 0 than start of source is taken fully from start (0) of the respected axis. If the start of the canvas is placed in negatives the source is cropped towards the positive start of the canvas to secure the orgin of the canvas so the image doesnt overflow.
+    source_x1 = max(0,-start_x) 
+    source_y1 = max(0,-start_y)
+    #takes distance from the start to the respected axis difference hense the dimensions
+    source_x2 = source_x1 + (x2-x1) 
+    source_y2 = source_y1 + (y2-y1)
+
+
+    empty_cavas[y1:y2 , x1:x2] = resize[source_y1:source_y2, source_x1:source_x2]# y means the rows and x means the columns 
+    # the differene dely and delx is taken in resize to fit exact amount of data from 0 to length of empty canvas.
+
+    normalize = empty_cavas.astype(np.float64)/255.0
+
+    return normalize.reshape(1,784) # production format
