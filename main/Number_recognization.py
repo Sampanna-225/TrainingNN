505be5449f54ac7e14bb3aa1ee2769ebbc0d0213
Sampanna-225 <sampanna339@gmail.com>
2026-06-05 21:45:14 +0545

    Managed timeline

--- main/Number_recognization.py
+++ main/Number_recognization.py
@@ -30,6 +30,10 @@ from data.unzip import X_image
 from data.unzip import X_image_test
 from data.unzip import Y_label
 from data.unzip import y_label_test
+from data.unzip import X_titanic
+from data.unzip import Y_titanic
+from data.unzip import a
+from data.unzip import f
 from main.image import image_converter
 # Assigning a thread event
 stop_spinning = threading.Event()
@@ -58,16 +62,24 @@ stop_spinning = threading.Event()
 #                [0,0,0,0,0,0,0,1,0,0],
 #                [0,0,0,0,0,0,0,0,1,0],
 #                [0,0,0,0,0,0,0,0,0,1]]) # from 1 to 10
-x1 =X_image
-y1 =Y_label
+
 sets = [1,2,3,4,5,6,7,8,9] # for normal number guessing
 sets_adv = [0,1,2,3,4,5,6,7,8,9] # for MINST label
+titanic_sets =["They Will Not Survive.", "They Will Survive"]
 
-model = NeuralBrain(x1,y1,input_size=784 , hidden_size=128 , output_size=10,lr=0.1)
+x1 =X_image
+y1 =Y_label
+
+x2 =X_titanic
+y2 =Y_titanic
+
+model_image = NeuralBrain(x1,y1,input_size=784 , hidden_size=128 , output_size=10,lr=0.1)
+model_prediction = NeuralBrain(x2,y2,input_size=6 , hidden_size=4 , output_size=2,lr=0.1)
+loading_screen()
 while(True):
-    os.system('cls')
-    loading_screen()
-    print("Here are the options:\n1) Train file\n2) Run Trained file\n3) Remove Trained Data\n4) To Exit")
+    
+    
+    print("Here are the options:\n1) Train Image (BETA)\n2 Train Titanic Survival Prediction \n3) Run Trained file\n4) Remove Trained Data\n5) To Exit")
     try:
         ask = int(input("Your Answer: "))
     except:
@@ -80,46 +92,93 @@ while(True):
             # t = threading.Thread(target=parallel_screen,args=(stop_spinning,)) # function and set args
             # t.start() # start spinning animation
             # try:
-            model.train(lo)
+            model_image.train(lo)
             # finally:
             #     stop_spinning.set() # Returns True for while loop to stop
             #     t.join() # Ensures the seperated threads join to acts on the main 
             
-            model.learn()
-            model.graphplot()
+            model_image.learn()
+            model_image.graphplot()
             continue
 
-        
         case 2:
-            model.remember()
+            lo =int(input("Enter training epoch: "))
+            model_prediction.train(lo)
+            model_prediction.learn()
+            model_prediction.graphplot()
+
+
+        case 3:
+            os.system('cls')
+            print("1) Pass Image\n2)Pass Titanic Prediction")
+            ask = int(input("Whats you choice: "))
+            model_image.remember()
             try:
-               a =input("Enter a image number: ")
-               test_img = os.path.join(data_folder,f"{a}.png")
-               temp =  image_converter(test_img)
-               
-               answer = sets_adv[np.argmax(model.forward_propagation(temp))]
-               print(f"\n The Prediction is {answer}\n")
-               print(model.forward_propagation(temp))
-               print(test_img)
+                if ask == 1:
+                    a =input("Enter a image number: ")
+                    test_img = os.path.join(data_folder,f"{a}.png")
+                    temp =  image_converter(test_img)
+
+                    answer = sets_adv[np.argmax(model_image.forward_propagation(temp))]
+                    print(f"\n The Prediction is {answer}\n")
+                    print(model_image.forward_propagation(temp))
+                    print(test_img)
+                elif ask == 2:
+                    pclass = int(input("Enter 1 "))
+                    pclass = pclass if (pclass <= 3 and pclass >=1) else 1
+                    sex = int(input("Enter Sex (1-Female/0-Male): "))
+                    sex = sex if (sex>=0 and sex<=1 ) else 0
+                    age = int(input("Enter age : "))
+                    age = age if age > 0 else (-age+1)
+                    fare = float(input("Enter fare: "))
+                    fare = fare if fare>0 else (-fare +1)
+                    pclass_encoding = [0.0,0.0,0.0]
+                    pclass_encoding[int(pclass)-1] = 1.0
+
+                    age = (age-min(a))/(max(a)-min(a))
+                    age = age if age >= 0 else 0.0 #if the minmax really cause trouble
+                    fare = (fare-min(f))/(max(f)-min(f))
+                    fare = fare if fare >=0 else 0.0 #just incase negative
+                    take = [[pclass_encoding[0],pclass_encoding[1],pclass_encoding[2],sex,age,fare]]
+                    takem = np.array(take,dtype=np.float32)
+                    answer = titanic_sets[np.argmax(model_prediction.forward_propagation(takem))]
+
+
+
+                else:
+                    print("Wrong Input: ")
+                    time.sleep(1)  
                
             except FileNotFoundError as e:
                 print(e)
                 
-        case 3:
+        case 4:
+            os.system('cls')
+            ask = int(input("Press 1 for Image file deletion\nPress 2 for Titanic Prediction file deletion"))
             
             try:
-                 ask = int(input("Do you really want to delete(1/0) : "))
-                 if ask == 1:
-                    loading_screen()
-                    model.forget()
-                    continue
+                if ask == 1 :
+                    ask = int(input("Do you really want to delete Image file(1/0) : "))
+                    if ask == 1:
+                        loading_screen()
+                        model_image.forget()
+                        continue
+                elif ask == 2:
+                    ask = int(input("Do you really want to delete Prediction file(1/0) : "))
+                    if ask == 1:
+                        loading_screen()
+                        model_prediction.forget()
+                        continue
+                else :
+                    print("Wrong Input: ")
+                    time.sleep(1)
                  
             except:
                  print("Enter a number !!!!!")
 
             
         
-        case 4:
+        case 5:
             dot_animation()
             break
 
@@ -136,4 +195,5 @@ while(True):
 
     except:
         continue
+    os.system('cls')
 
